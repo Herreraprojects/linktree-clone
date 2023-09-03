deac63093bd07a2d20f230db3788c080f08a258e
Herreraprojects <119438944+Herreraprojects@users.noreply.github.com>
2023-09-03 17:15:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var groceries = ['eggs','milk','cheese'];
+var items = ['item1','item2','item3'];
 
-function listgroceries(){
-    for(var i=0; i< groceries.length; i++){
-        console.log(groceries[i]);
+function listitems(){
+    for(var i=0; i< items.length; i++){
+        console.log(items[i]);
 
     }
 }
 
-listgroceries();
+listitems();
 
 document.getElementById('box').addEventListener('click', function(){
     alert('I got clicked');
